0be5be487ba4d15a4bde3cccc1688eb228319456
Alidabi10 <112751131+Alidabi10@users.noreply.github.com>
2025-10-30 22:34:19 +0100

    Klasa ResourceManager
    Implemented ResourceManager class with resource management and copy semantics.

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,45 @@
 #pragma once
 
 #include "Resource.hpp"
-
 class ResourceManager
-{
-    // Twoja implementacja tutaj
+{ 
+	//resource to jakby robot
+	//resourceManager to pilot do robota
+private:
+	Resource* resource;
+
+public:
+	ResourceManager()
+	{
+		resource = new Resource();
+	}
+	~ResourceManager()
+	{
+		delete resource;
+	}
+
+	//konstruktor kopiujacy
+	ResourceManager(const ResourceManager& other) 
+	{
+		resource = new Resource(*other.resource);
+	}
+
+	//operator przypisania kopiujacego =
+	ResourceManager& operator=(const ResourceManager& other)
+	{
+		if (this != &other)
+		{
+			Resource* tmp = new Resource(*other.resource);
+			delete resource;
+			resource = tmp;
+		}
+		return *this;
+	}
+	double get() const 
+	{
+		return (*resource).get();
+	}
+
+
 };
+
